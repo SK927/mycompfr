a7e1311d84b33313d3dee03046648d06c6868194
SK927 <m.lefebv@gmail.com>
2024-07-08 18:08:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,61 +13,57 @@
 
 	<body>  
     <div class="container text-center">
-      <h3 class="col mt-4 font-weight-bold">Select the desired tool</h3>
-      <div class="row mt-4 mb-2">
-        <a class="col-auto mx-auto" href="https://registration-checker.mycomp.fr" target="_blank">
-          <button class="btn btn-light btn-lg">Registration Checker</button>
-        </a>
-      </div>
-      <div class="row mb-4 justify-content-center">
-        <div class="col-6">
-          Ask competitors to confirm their presence at your competition
-        </div>
-      </div>
-      <div class="row mb-2">
-        <a class="col-auto mx-auto" href="https://registration-handler.mycomp.fr" target="_blank">
-          <button class="btn btn-light btn-lg">Registration Handler</button>
-        </a>
-      </div>
-      <div class="row mb-4 justify-content-center">
-        <div class="col-6">
-          Handle on the spot registrations easily by letting competitors register and by generating the required scorecards
-        </div>
-      </div>
-      <div class="row mb-2">
-        <a class="col-auto mx-auto" href="https://67gen.mycomp.fr" target="_blank">
-          <button class="btn btn-light btn-lg">67 Gen</button>
-        </a>
-      </div>
-      <div class="row mb-4 justify-content-center">
-        <div class="col-6">
-          Generate dual scorecards for 6x6 / 7x7 or 4bld / 5bld when both events share the same cumulative
-        </div>
-      </div>
-      <div class="row mb-2">
-        <a class="col-auto mx-auto" href="https://commande-utile.mycomp.fr" target="_blank">
-          <button class="btn btn-light btn-lg">Commande Utile</button>
-        </a>
-      </div>
-      <div class="row mb-4 justify-content-center">
-        <div class="col-6">
-          Handle products orders for your competition (French only)
-        </div>
-      </div>
-      <div class="row mb-2">
-        <a class="col-auto mx-auto" href="https://pimp-my-faq.mycomp.fr" target="_blank">
-          <button class="btn btn-light btn-lg">Pimp My FAQ</button>
-        </a>
-      </div>
-      <div class="row mb-4 justify-content-center">
-        <div class="col-6">
-          Generate the FAQ for AFS sanctioned competitions from competition URL
-        </div>
-      </div>
+      <table class="table table-striped mt-5">
+        <thead>
+          <tr>
+            <th scope="col">Tool</th>
+            <th scope="col">Description</th>
+            <th scope="col">Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Registration Checker</td>
+            <td>Ask competitors to confirm their presence at your competition</td>
+            <td><a class="link-success" href="https://registration-checker.mycomp.fr" target="_blank">here</a></td>
+          </tr>
+          <tr>
+            <td>Registration Handler</td>
+            <td>Handle on the spot registrations easily by letting competitors register and by generating the required scorecards</td>
+            <td><a class="link-success" href="https://registration-handler.mycomp.fr" target="_blank">here</a></td>
+          </tr>
+          <tr>
+            <td>67 Gen</td>
+            <td>Generate dual scorecards for 6x6 / 7x7 or 4bld / 5bld when both events share the same cumulative</td>
+            <td><a class="link-success" href="https://67gen.mycomp.fr" target="_blank">here</a></td>
+          </tr>
+          <tr>
+            <td>Commande Utile</td>
+            <td>Handle items orders for your competition (French only)</td>
+            <td><a class="link-success" href="https://commande-utile.mycomp.fr" target="_blank">here</a></td>
+          </tr>
+          <tr>
+            <td>Pimp My FAQ</td>
+            <td>Generate the FAQ for AFS sanctioned competitions from competition URL (French only)</td>
+            <td><a class="link-success" href="https://pimp-my-faq.mycomp.fr" target="_blank">here</a></td>
+          </tr>
+          <tr>
+            <td>(no)Show</td>
+            <td>Generate the list of no-shows for a competition, after the competition ended</td>
+            <td><a class="link-success" href="https://mycomp.fr/noshow" target="_blank">here</a></td>
+          </tr>
+          <tr>
+            <td>COMPare</td>
+            <td>Compare registrants list between two competitions</td>
+            <td><a class="link-success" href="https://mycomp.fr/compare" target="_blank">here</a></td>
+          </tr>
+        </tbody>
+      </table>
+
       <footer class="py-3 my-4">
         <ul class="nav justify-content-center pb-3 mb-3">
           <li class="nav-item">
-            <a href="https://mycomp.fr" class="nav-link px-2 text-muted">© My Comp 2021-<?php echo date( 'Y' ); ?></a>
+            <a href="#" class="nav-link px-2 text-muted">© My Comp 2021-<?php echo date( 'Y' ); ?></a>
           </li>
           <li class="nav-item">
             <a href="mailto:mlefebvre@worldcubeassociation.org" class="nav-link px-2 text-muted">Maxime Lefebvre</a>
