8625da2dbdc75d9052f55f5308f97edbfd429458
SK927 <96429132+SK927@users.noreply.github.com>
2025-10-12 15:28:35 +0200

    Update pdf_generate-orders-list-by-day.php
    Added total amount of orders to the PDF

--- commande-utile/src/pdf_generate-orders-list-by-day.php
+++ commande-utile/src/pdf_generate-orders-list-by-day.php
@@ -24,25 +24,43 @@
       $pdf->AddPage();
 
   	  $html = "<table cellpadding=\"10\">";
-      $html .= "<tr><td colspan=\"2\" style=\"border:1px solid #808080;background-color:#e4e4e4\"><b>{$block['name']}</b></td></tr>";
+      $html .= "<tr><td colspan=\"2\" style=\"width:90%;border:1px solid #808080;background-color:#e4e4e4\"><b>{$block['name']}</b></td>";
+      $html .= "<td style=\"width:10%;border:1px solid #808080;background-color:#e4e4e4\"><b>Total jour</b></td></tr>";
       $html .= "</table>";
 	  
       $pdf->WriteLine( $html );
-	  
+
       foreach ( $competition_orders as $order )
       { 
+        $sum = 0;
+
         $order_data = from_pretty_json( $order['order_data'] );
 
 		    if ( $order_data[ $block_key ]['items'] )
         {
+          $total = number_format( $order['order_total'], 2 );
     		  $html = "<table cellpadding=\"10\"><tr>";
-          $html .= "<td style=\"width:20%;border:1px solid #808080\">{$order['user_name']}</td>";
-    		  $html .= "<td style=\"width:80%;border:1px solid #808080\"><ul>";
+
+
+          if ( $order['user_comment'] )
+          {
+            $html .= "<td rowspan=\"2\" style=\"width:20%;border:1px solid #808080\">{$order['user_name']} ({$total}&nbsp;€)</td>";
+            $html .= "<td colspan=\"2\" bgcolor=\"#fdffc9\" style=\"border: 1px solid #808080;width:80%\">{$order['user_comment']}</td>
+                    </tr>
+                    <tr>";
+          }      
+          else
+          {
+            $html .= "<td style=\"width:20%;border:1px solid #808080\">{$order['user_name']} ({$total}&nbsp;€)</td>";
+          }
+
+    		  $html .= "<td style=\"width:70%;border:1px solid #808080\"><ul>";
 
     		  $order_data = from_pretty_json( $order['order_data'] );
 		         
           foreach ( $order_data[ $block_key ]['items'] as $item_key => $item )
           {
+            $sum += $item['qty'] * $catalog[ $block_key ]['items'][ $item_key ]['price'];
             $html .= "<li>{$item['qty']} x {$catalog[ $block_key ]['items'][ $item_key ]['name']}";
             $options = array();
 
@@ -61,6 +79,7 @@
 
             foreach ( $options as $key => $qty )
             {
+              $sum += $qty * $catalog[ $block_key ]['items'][ $item_key ]['options'][ $option_key ]['selections'][ $selection_key ]['price'];
               [ $option_key, $selection_key ] = explode( '_', $key );
               array_push( $temp_array, "{$qty} x {$catalog[ $block_key ]['items'][ $item_key ]['options'][ $option_key ]['selections'][ $selection_key ]['name']}" );
             }
@@ -73,7 +92,9 @@
 
             $html .= '</li>';
           }		  
+          $sum = number_format( $sum, 2 );
           $html .= "</ul></td>";
+          $html .= "<td style=\"width:10%;border:1px solid #808080\">{$sum} €</td>";
           $html .= "</tr></table>";
 
           $pdf->WriteLine( $html );
